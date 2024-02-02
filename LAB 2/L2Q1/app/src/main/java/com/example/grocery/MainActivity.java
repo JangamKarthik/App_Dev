de58c8621c9454f6c351ee62e6d3636c8ed410c5
Karthik_Jangam <102133551+JangamKarthik@users.noreply.github.com>
2024-02-03 00:08:01 +0530

    Update MainActivity.java

--- LAB 2/L2Q1/app/src/main/java/com/example/grocery/MainActivity.java
+++ LAB 2/L2Q1/app/src/main/java/com/example/grocery/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView1 = findViewById(R.id.recyclerView);
         buttonSubmit = findViewById(R.id.buttonSubmit);
 
-        // Initialize the list of grocery items
         groceryItemList = new ArrayList<>();
         groceryItemList.add(new GroceryItem("Item 1"));
         groceryItemList.add(new GroceryItem("Item 2"));
@@ -43,12 +42,10 @@ public class MainActivity extends AppCompatActivity {
         groceryItemList.add(new GroceryItem("Item 9"));
         groceryItemList.add(new GroceryItem("Item 10"));
 
-        // Set up the RecyclerView
         groceryAdapter = new GroceryAdapter(groceryItemList);
         recyclerView1.setLayoutManager(new LinearLayoutManager(this));
         recyclerView1.setAdapter(groceryAdapter);
 
-        // Set up the click listener for the Submit button
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
                 selectedItems.append(item.getName()).append("\n");
             }
         }
-        // Display the selected items in a Toast
         Toast.makeText(this, selectedItems.toString(), Toast.LENGTH_LONG).show();
     }
 }
