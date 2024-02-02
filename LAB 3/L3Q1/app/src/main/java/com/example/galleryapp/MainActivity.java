b8f3b4aebf3c20693ba0bc5e99c5485a862d5fd0
Karthik_Jangam <102133551+JangamKarthik@users.noreply.github.com>
2024-02-03 00:10:04 +0530

    Update MainActivity.java

--- LAB 3/L3Q1/app/src/main/java/com/example/galleryapp/MainActivity.java
+++ LAB 3/L3Q1/app/src/main/java/com/example/galleryapp/MainActivity.java
@@ -22,17 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                // Display a toast message with the selected image
                 Toast.makeText(getApplicationContext(), "Image " + (position + 1), Toast.LENGTH_SHORT).show();
 
-                // Obtain the resource ID of the selected image
                 int selectedImageId = ((ImageAdapter) parent.getAdapter()).getItem(position);
 
-                // Display the full-size image in an ImageView
                 ImageView imageView = new ImageView(MainActivity.this);
                 imageView.setImageResource(selectedImageId);
 
-                // For simplicity, let's just show the image in a larger toast
                 Toast toast = new Toast(getApplicationContext());
                 toast.setView(imageView);
                 toast.show();
